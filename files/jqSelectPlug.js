e33ce62f6b5387c667b3433f102b0a3803b0521a
KnightsForce <andrianov44@gmail.com>
2014-08-20 15:21:16 +0300

    v 2.0

--- files/jqSelectPlug.js
+++ files/jqSelectPlug.js
@@ -5,45 +5,53 @@ $(document).ready(function(){
 
 var select = $('.jquery-select-plug');
 var lis = select.children('li');
+var heightSelect=select.height();
+var firstLiHeight = lis.eq(0).outerHeight(true);
 var count=false;
+var list;
 
-
-function addHover(lis) {
-lis.not(':first').addClass('afterFirst');
-}
+select.css('height', firstLiHeight+'px');
 
 addHover(lis);
 
-
-
+//$(e.target).parent()!=select
 $(document.body).click(function(e){
 
-if(e.target.tagName != "LI") {
-if(!count) return;
-select.animate({'height': '40px'}, {'duration': 500});
-count=false;
-return;
-};
+	if($(e.target).closest(select).length<1) { /* если не li. closest вместо parent 
+												  для возможности дополнительного оборачивания неизвестно зачем.*/
+		if(!count) return;
+	
+		select.stop().animate({'height': firstLiHeight+'px'}, {'duration': 500});
+		count=false;
+		return;
+	};
+
 	if(!count) {
-		select.animate({'height': '220px'}, {'duration': 500});
+		select.stop().animate({'height': heightSelect+'px'}, {'duration': 500});
 		count=true;
 		return;
 	}
-	select.animate({'height': '40px'}, {'duration': 500});
+	
+	select.stop().animate({'height': firstLiHeight+'px'}, {'duration': 500});
 	count=false;
 
 	select.append(lis).prepend(e.target);
 
-list = select.children('li');
+	list = select.children('li');
 
-list.removeClass('afterFirst');
+	list.removeClass('hoverAfterFirst');
 
-addHover(list)
+	firstLiHeight = list.eq(0).outerHeight(true); // если разная высота строк.
+
+	addHover(list);
+
 
 }); // конец click
-  
 
 
+function addHover(lis) {
+	lis.not(':first').addClass('hoverAfterFirst'); // добавление класса всем кроме первого для hover
+}
 
 }); // конец ready
 
